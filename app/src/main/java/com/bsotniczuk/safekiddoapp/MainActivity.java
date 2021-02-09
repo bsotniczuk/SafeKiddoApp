c3e0d3b8b32d19fd20abb9d88c5f5eb6d117b751
Bartlomiej <bsotniczuk@gmail.com>
2021-02-09 21:44:25 +0100

    Initial commit

---
+++ app/src/main/java/com/bsotniczuk/safekiddoapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.bsotniczuk.safekiddoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //TODO: test path
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //https://run.mocky.io/v3/6125f2d0-0688-4547-aae8-0295d984f196
+    }
+}
